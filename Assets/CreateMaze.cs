67e90240d677a2ef181ed1f2c67c04bfb4c43d1b
matig123 <matiasgrotewold11@gmail.com>
2016-09-03 18:45:09 -0400

    Code changes. Blocks randomly appear on "Space" click. Tried to implement collisions but failed

---
+++ Assets/CreateMaze.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateMaze : MonoBehaviour {
+	void Start(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			print("here");
+		}
+	}
+
+	void RestartGame(){
+		print ("here");
+		for (int j = 0; j < 20; j++) {
+			GameObject mazeWall = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			print (mazeWall);
+			mazeWall.transform.parent = transform;
+			mazeWall.name = "wall" + j;
+			mazeWall.transform.position = new Vector3 (Random.Range (-20.0f, 20.0f), 0.5f, Random.Range (-20.0f, 20.0f));
+			mazeWall.transform.localScale = new Vector3 (Random.Range (0.0f, 1.0f), 1.0f, Random.Range (0.0f, 1.0f));
+		}
+	}
+
+	void Destroy(){
+		/*	var objects = GameObject.FindObjectOfType (mazeWall);
+		foreach(GameObject o in objects){
+			Destroy(o);
+		}*/
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Destroy ();
+			RestartGame ();
+		}
+
+	}
+}
--- Assets/ObjectMove.cs
+++ Assets/ObjectMove.cs
@@ -4,9 +4,25 @@ using System.Collections;
 public class ObjectMove : MonoBehaviour {
 
 public float rate = 3f;
-	
+
+	void OnTriggerEnter(Collider other)
+	{
+		print("Collision detected with trigger object " + other.name);
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		print("Still colliding with trigger object " + other.name);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		print(gameObject.name + " and trigger object " + other.name + " are no longer colliding");
+	}
+
 	// Update is called once per frame
     void Update() {
+
         if (Input.GetKey(KeyCode.A)){
             transform.position += Vector3.left * rate * Time.deltaTime;
         }
@@ -20,7 +36,9 @@ public float rate = 3f;
         }
 
         if (Input.GetKey(KeyCode.S)){
+			
         	transform.position -= Vector3.forward * rate * Time.deltaTime;
         }
     }
+		
 }
