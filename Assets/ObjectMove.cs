ef2c81d2c6416b01f31fa96fd2ea22296ad1a8c9
Matt Cramblett <Cramblett.10@osu.edu>
2016-09-03 10:50:37 -0400

    initial commit

---
+++ Assets/ObjectMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectMove : MonoBehaviour {
+
+public float rate = 3f;
+	
+	// Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.A)){
+            transform.position += Vector3.left * rate * Time.deltaTime;
+        }
+        
+        if (Input.GetKey(KeyCode.D)){
+            transform.position += Vector3.right * rate * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W)){
+        	transform.position += Vector3.forward * rate * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S)){
+        	transform.position -= Vector3.forward * rate * Time.deltaTime;
+        }
+    }
+}
